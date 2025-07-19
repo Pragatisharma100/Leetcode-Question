3bbcbbf264763ab16b5dde2970d174e78c25bee8
Pragati Sharma <157017872+Pragatisharma100@users.noreply.github.com>
2025-07-19 09:25:46 +0530

    Time: 26 ms (77.15%) | Memory: 30.7 MB (60.37%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        res = []
+        for f in sorted(folder):
+            if not res or not f.startswith(res[-1] + "/"):
+                res.append(f)
+        return res
+
+                
