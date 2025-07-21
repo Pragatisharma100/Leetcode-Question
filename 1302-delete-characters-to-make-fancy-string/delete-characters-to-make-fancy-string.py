863d52b9c8e897939b7da55504e8e33a538e014e
Pragati Sharma <157017872+Pragatisharma100@users.noreply.github.com>
2025-07-21 14:47:11 +0530

    Time: 330 ms (59.84%) | Memory: 19.5 MB (76.94%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        cnt=1
+        if len(s) <3:
+            return s
+        ans=s[0]
+        for i in range(1, len(s)):
+            if s[i] ==s[i-1]:
+                cnt+=1
+            else:
+                cnt=1
+            if cnt<3:
+                ans+=s[i]
+        return ans
