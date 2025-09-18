bea37ff6cdbc6f4af64040c1b31e7fb0e5a40b97
Pragati Sharma <157017872+Pragatisharma100@users.noreply.github.com>
2025-09-18 22:22:31 +0530

    Time: 106 ms (85.59%) | Memory: 59.2 MB (82.39%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -2,20 +2,19 @@ class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         ans1=[]
         ans2=[]
-        players=set()
-        loser=[j for i,j in matches]
-        l=set(loser)
-        freq=Counter(loser)
-        for key,val in freq.items():
+        d={}
+        for i,j in matches:
+            if i not in d:
+                d[i]=0
+            if j not in d or d[j]==0:
+                d[j]=1
+            elif d[j]==1:
+                d[j] +=1
+        for key,val in d.items():
+            if val==0:
+                ans1.append(key)
             if val==1:
                 ans2.append(key)
-        for i in range(len(matches)):
-            players.add(matches[i][0])
-            players.add(matches[i][1])
-        players=list(players)
-        for j in players:
-            if j not in l:
-                ans1.append(j)
         ans1.sort()
         ans2.sort()
         return [ans1,ans2]
